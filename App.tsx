394fc0dac759c2773e9af32ab6f5464b1c8fc191
lukakkyk <125033834+lukakkyk@users.noreply.github.com>
2024-01-11 16:49:36 +0400

    laoding animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { Text, View } from "react-native";
+import { MotiView } from "moti";
+const LoadingIndicator = ({ size }: { size: number }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <MotiView
+      from={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        borderWidth: 0,
+        shadowOpacity: 0.5,
+      }}
+      animate={{
+        width: size + 20,
+        height: size + 20,
+        borderRadius: (size + 20) / 2,
+        borderWidth: size / 10,
+        shadowOpacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 1000,
+        // repeat:Infinity
+        loop: true,
+      }}
+      style={{
+        height: size,
+        width: size,
+        borderRadius: size / 2,
+        borderWidth: size / 10,
+        borderColor: "#fff",
+        shadowColor: "#fff",
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+      }}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#010101",
+      }}
+    >
+      <LoadingIndicator size={100} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
